8b1617c9ee4538682035b5ea2e42e98207c47ba1
AjayDaule <70071276+AjayDaule@users.noreply.github.com>
2020-08-30 19:47:48 +0530

    Add files via upload

---
+++ Stopwatch.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    class Stopwatch
+    {
+        public DateTime starttime;
+        public DateTime stoptime;
+       
+        public bool isStarted;
+
+
+        public void stopwatch()
+        {
+            starttime = new DateTime();
+            stoptime = new DateTime();
+            isStarted = false;
+
+        }
+        public void start()
+        {
+            if (isStarted)
+                throw new InvalidOperationException();
+
+            starttime = DateTime.Now;
+            Console.WriteLine("StartTime:"+starttime.Hour+":"+starttime.Minute+":"+starttime.Second+":"+starttime.Millisecond);
+        }
+        public void stop()
+        {
+
+            stoptime = DateTime.Now;
+            isStarted = false;
+            Console.WriteLine("StopTime:"+stoptime.Hour+":"+stoptime.Minute+":"+stoptime.Second+":"+stoptime.Millisecond);
+            TimeSpan duration = stoptime - starttime;
+        }
+       
+            private TimeSpan Interval;
+
+            public TimeSpan MyProperty
+            {
+               get { Interval = stoptime - starttime;
+                      return Interval;}
+             
+ 
+         }
+    }
+    
+}
